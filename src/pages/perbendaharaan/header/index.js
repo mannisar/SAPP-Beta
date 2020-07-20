4d56190cdb2db8511ae3011138a921dca960643f
hilmimuharromi <hilmimuharrom@gmail.com>
2020-07-20 14:50:21 +0700

    responsive

--- src/pages/perbendaharaan/header/index.js
+++ src/pages/perbendaharaan/header/index.js
@@ -1,36 +1,58 @@
-import { React, Input, Select, Row, Col, Button, PrinterFilled, PlusOutlined, useHistory } from '../libraries/dependencies';
+import {
+  React,
+  Input,
+  Select,
+  Row,
+  Col,
+  Button,
+  PrinterFilled,
+  PlusOutlined,
+  useHistory,
+} from "../libraries/dependencies";
 
 const { Option } = Select;
 const { Search } = Input;
 
 function Head() {
-    let history = useHistory();
-    const handleNavigate = () => {
-        history.push('/Perekaman')
-    };
-    return (
-        <Row justify='start' style={{ backgroundColor: 'gray', padding: 8 }}>
-            <Col span={8}>
-                <div className="site-input-group-wrapper">
-                    <Input.Group compact>
-                        <Select defaultValue="keys" style={{ width: 175 }}>
-                            <Option value="npwp">npwp</Option>
-                            <Option value="status">status</Option>
-                        </Select>
-                        <Search
-                            placeholder="Search.."
-                            onSearch={value => console.log(value)}
-                            style={{ width: 200, marginLeft: 14 }}
-                        />
-                    </Input.Group>
-                </div>
-            </Col>
-            <Col span={4}>
-                <Button type="danger" icon={<PlusOutlined />} size={'middle'} onClick={handleNavigate} />
-                <Button type="info" icon={<PrinterFilled />} size={'middle'} style={{ marginLeft: 8 }} />
-            </Col>
-        </Row>
-    )
+  let history = useHistory();
+  const handleNavigate = () => {
+    history.push("/Perekaman");
+  };
+  return (
+    <Row justify="start" style={{ backgroundColor: "gray", padding: 8 }}>
+      <Col span={5}>
+        <div className="site-input-group-wrapper">
+          <Input.Group compact>
+            <Select defaultValue="keys" style={{ width: 200 }}>
+              <Option value="npwp">npwp</Option>
+              <Option value="status">status</Option>
+            </Select>
+          </Input.Group>
+        </div>
+      </Col>
+      <Col span={5}>
+        <Search
+          placeholder="Search.."
+          onSearch={(value) => console.log(value)}
+          style={{ width: 200 }}
+        />
+      </Col>
+      <Col span={4}>
+        <Button
+          type="danger"
+          icon={<PlusOutlined />}
+          size={"middle"}
+          onClick={handleNavigate}
+        />
+        <Button
+          type="info"
+          icon={<PrinterFilled />}
+          size={"middle"}
+          style={{ marginLeft: 8 }}
+        />
+      </Col>
+    </Row>
+  );
 }
 
 export default Head;
